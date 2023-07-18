3879cd402f4b5a6b60d0340b7f5536213eb8703b
Jesse Robertson <jesse.robertson360@gmail.com>
2023-07-18 17:12:40 -0300

    Commit before removing AddCar() from external file
    I am going to merge the files and remake the design now to make an inventory system. It will change the structure quite a bit so this is the last of the seperated function.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import { AddCar } from "./AddCar.js"
+import AddCar from "./AddCar.js";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <Title />
-      <AddCar />
     </div>
   );
 }
 
-function Title(){
-  const title = "Virtual Galery.";
+function Title() {
+  const title = "Virtual Gallery.";
   const showTitle = true;
 
-  return(
+  return (
     <div>
       <h1> {showTitle ? title : "None"} </h1>
       {/* CONDITION ? DoTHIS : ELSE */}
       <p> This is where we can list the cars </p>
+      <AddCar text="Enter Car" number={2} />
       <AddCar />
     </div>
   );
 }
 
-export default App();
+export default App;
