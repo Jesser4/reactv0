7dd746498dd369c64ecee76344dc3ec36857e302
Jesse Robertson <jesse.robertson360@gmail.com>
2023-07-21 17:12:47 -0300

    New branch is up to date
    There were file conflicts after the merge for some reason

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SearchBar from "./SearchBar";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+
+  const updateData = (searchParams) => {
+    setData(searchParams);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar callback={updateData} />
+      <p>Name: {"name" in data ? data["name"] : "No name to display"}</p>
+      <p>Year: {"year" in data ? data["year"] : "No year to display"}</p>
+      <p>Brand: {"brand" in data ? data["brand"] : "No brand to display"}</p>
+      <p>
+        Champion:{" "}
+        {"champion" in data
+          ? data["champion"].toString()
+          : "No champion status"}{" "}
+      </p>
     </div>
   );
 }
